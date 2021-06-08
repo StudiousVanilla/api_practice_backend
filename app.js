6a07f7e3ad0e6d1268673fef5b676dec8c6419ea
Oisín_windows <byrne.ois@gmail.com>
2021-06-08 17:56:53 +0100

    tidied up the graphql requests

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express()
 const port = process.env.PORT || 3000;
 const cors = require('cors')
 
-
 const corsOptions = {
   origin: ["http://localhost:3001","http://localhost:3000" ,"https://hungry-goodall-28f444.netlify.app"]
 }
@@ -14,58 +13,25 @@ app.use(cors(corsOptions))
 // middleware
 app.use(express.json())
 
-
-
-
-
-
-
-
-
-// // Add headers
-// app.use(function (req, res, next) {
-//     const allowedOrigins = ["https://hungry-goodall-28f444.netlify.app", "http://localhost:3001", "http://localhost:3000"];
-//     const origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//        res.setHeader('Access-Control-Allow-Origin', origin);
-//     }
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-
-
-//     res.setHeader('set-cookie', 'samesite=None; secure');
-
-//     // Pass to next layer of middleware
-//     next();
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
 // initialise routes
 const LOTRRoutes = require("./routes/LOTRRoutes")
 const cocktailRoutes = require("./routes/cocktailRoutes")
 
+
+
+
+
+
+// LOTR ****
+
 // use LOTR routes for base URL endpoint
 app.use('/', LOTRRoutes)
 
+
+
+
+
+// cocktails ****
 
 
 // get schema for graphQL
@@ -77,14 +43,14 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 
-
-
-
-
-
 // use cocktail routes for base URL endpoint
 // may not be necessary due to graphQL
 app.use('/cocktail', cocktailRoutes)
+
+
+
+
+// Port ****
 
 
 app.listen(port, () => {
--- graphQL.js
+++ graphQL.js
@@ -9,48 +9,49 @@ const {
   GraphQLInt} = require('graphql')
 
 
-// const DrinkInfoType = new GraphQLObjectType({
-//     name: 'DrinkInfo',
-//     description: 'extra drink information',
-//     fields: ()=>({
-//       strAlcoholic: {type:GraphQLString},
-//       strGlass: {type:GraphQLString},
-//       strInstructions: {type:GraphQLString},
-//       strIngredient1: {type:GraphQLString},
-//       strIngredient2: {type:GraphQLString},
-//       strIngredient3: {type:GraphQLString},
-//       strIngredient4: {type:GraphQLString},
-//       strIngredient5: {type:GraphQLString},
-//       strIngredient6: {type:GraphQLString},
-//       strIngredient7: {type:GraphQLString},
-//       strIngredient8: {type:GraphQLString},
-//       strIngredient9: {type:GraphQLString},
-//       strIngredient10: {type:GraphQLString},
-//       strIngredient11: {type:GraphQLString},
-//       strIngredient12: {type:GraphQLString},
-//       strIngredient13: {type:GraphQLString},
-//       strIngredient14: {type:GraphQLString},
-//       strIngredient15: {type:GraphQLString},
-//       strMeasure1: {type:GraphQLString},
-//       strMeasure2: {type:GraphQLString},
-//       strMeasure3: {type:GraphQLString},
-//       strMeasure4: {type:GraphQLString},
-//       strMeasure5: {type:GraphQLString},
-//       strMeasure6: {type:GraphQLString},
-//       strMeasure7: {type:GraphQLString},
-//       strMeasure8: {type:GraphQLString},
-//       strMeasure9: {type:GraphQLString},
-//       strMeasure10: {type:GraphQLString},
-//       strMeasure11: {type:GraphQLString},
-//       strMeasure12: {type:GraphQLString},
-//       strMeasure13: {type:GraphQLString},
-//       strMeasure14: {type:GraphQLString},
-//       strMeasure15: {type:GraphQLString},
-//     })
-// })
+// gives all the information about a drink. Used when displaying a single drink
+const FullDrinkType = new GraphQLObjectType({
+    name: 'DrinkInfo',
+    description: 'extra drink information',
+    fields: ()=>({
+      strAlcoholic: {type:GraphQLString},
+      strGlass: {type:GraphQLString},
+      strInstructions: {type:GraphQLString},
+      strIngredient1: {type:GraphQLString},
+      strIngredient2: {type:GraphQLString},
+      strIngredient3: {type:GraphQLString},
+      strIngredient4: {type:GraphQLString},
+      strIngredient5: {type:GraphQLString},
+      strIngredient6: {type:GraphQLString},
+      strIngredient7: {type:GraphQLString},
+      strIngredient8: {type:GraphQLString},
+      strIngredient9: {type:GraphQLString},
+      strIngredient10: {type:GraphQLString},
+      strIngredient11: {type:GraphQLString},
+      strIngredient12: {type:GraphQLString},
+      strIngredient13: {type:GraphQLString},
+      strIngredient14: {type:GraphQLString},
+      strIngredient15: {type:GraphQLString},
+      strMeasure1: {type:GraphQLString},
+      strMeasure2: {type:GraphQLString},
+      strMeasure3: {type:GraphQLString},
+      strMeasure4: {type:GraphQLString},
+      strMeasure5: {type:GraphQLString},
+      strMeasure6: {type:GraphQLString},
+      strMeasure7: {type:GraphQLString},
+      strMeasure8: {type:GraphQLString},
+      strMeasure9: {type:GraphQLString},
+      strMeasure10: {type:GraphQLString},
+      strMeasure11: {type:GraphQLString},
+      strMeasure12: {type:GraphQLString},
+      strMeasure13: {type:GraphQLString},
+      strMeasure14: {type:GraphQLString},
+      strMeasure15: {type:GraphQLString},
+    })
+})
 
-
-const DrinkNumIngredients = new GraphQLObjectType({
+// gives basic information about a drink. Used when displaying list off drinks
+const BasicDrinkInfo = new GraphQLObjectType({
   name: 'numIngredients',
   description: 'extra drink information',
   fields: ()=>({
@@ -70,15 +71,16 @@ const DrinkNumIngredients = new GraphQLObjectType({
     }
   })
 })
-  
-const DrinkType = new GraphQLObjectType({
+
+// gives basic drink information based on an id
+const BasicDrinkType = new GraphQLObjectType({
     name: 'Drink',
     description: 'basic drink information',
     fields: ()=>({
       strDrink:{ type: GraphQLNonNull(GraphQLString)},
       idDrink: { type: GraphQLNonNull(GraphQLString)},
       drinkInfo: {
-      type: DrinkNumIngredients,
+      type: BasicDrinkInfo,
       resolve: async (drink) => {
         const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/${process.env.COCKTAIL_API_KEY}/lookup.php?i=${drink.idDrink}`)
   
@@ -89,13 +91,15 @@ const DrinkType = new GraphQLObjectType({
     },
     })
 })
-  
+
+
 const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: ()=> ({
+      // used when getting a list of drinks and their basic info
       drinks:{
-        type: new GraphQLList(DrinkType),
+        type: new GraphQLList(BasicDrinkType),
         description: 'List of drinks',
         args: {
           ingredients: { type: GraphQLString}
@@ -107,8 +111,9 @@ const RootQueryType = new GraphQLObjectType({
           return data.drinks
         }
       },
+      // used when getting a single drink and all its info
       drink:{
-        type: DrinkType,
+        type: FullDrinkType,
         description: 'A single drink',
         args: {
           id: { type: GraphQLString}
@@ -121,20 +126,6 @@ const RootQueryType = new GraphQLObjectType({
           return data.drinks[0]
         }
       },
-      test:{
-        type: DrinkType,
-        description: 'A single drink',
-        args: {
-          id: { type: GraphQLString}
-        },
-        resolve: async () => {
-          const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/${process.env.COCKTAIL_API_KEY}/lookup.php?i=15849`)
-  
-          const data = await response.json()
-          
-          return data.drinks[0]
-        }
-      }
     })
 })
   
